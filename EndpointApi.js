d4c623b82bf25a68c47428be301f2e70038983f1
Chinna-Anaparthi <123636616+Chinna-Anaparthi@users.noreply.github.com>
2023-11-30 18:01:53 +0530

    api's modification stage - 1

--- EndpointApi.js
+++ EndpointApi.js
@@ -8,104 +8,104 @@ app.use(cors());
 const port = 2000;
 
 //Admin-Data
-app.post("/admin/RegisterKPI", (req, res, next) => {
+app.post("/admin/register", (req, res, next) => {
   Server_Logic.AdminPost(req, res, () => {});
 });
-app.post("/admin/LoginKPI", (req, res, next) => {
+app.post("/admin/login", (req, res, next) => {
   Server_Logic.AdminloginPost(req, res, () => {});
 });
-app.post("/admin/Employee", (req, res, next) => {
+app.post("/admin/emp_insrt", (req, res, next) => {
   Server_Logic.Admin_Employee_Insert_Data(req, res, () => {});
 });
-app.get("/admin/Employee", (req, res, next) => {
+app.get("/admin/emp_data", (req, res, next) => {
   Server_Logic.Admin_Employee_Retrive_Data(req, res, () => {});
 });
 app.delete(
-  "/admin/Employee/:adminID/:category?/:name?/:questions?",
+  "/admin/emp_del/:adminID/:category?/:name?/:questions?",
   (req, res, next) => {
     Server_Logic.Admin_Employee_Delete_Data(req, res, () => {});
   }
 );
 
-app.post("/admin/Manager", (req, res, next) => {
+app.post("/admin/manager_insrt", (req, res, next) => {
   Server_Logic.Admin_Manager_Insert_Data(req, res, () => {});
 });
-app.get("/admin/Manager", (req, res, next) => {
+app.get("/admin/manager_data", (req, res, next) => {
   Server_Logic.Admin_Manager_Retrive_Data(req, res, () => {});
 });
 app.delete(
-  "/admin/Manager/:adminID/:category?/:name?/:questions?",
+  "/admin/manager_del/:adminID/:category?/:name?/:questions?",
   (req, res, next) => {
     Server_Logic.Admin_Manager_Data_Delete(req, res, () => {});
   }
 );
 
-app.post("/admin/Director", (req, res, next) => {
+app.post("/admin/director_insrt", (req, res, next) => {
   Server_Logic.Admin_Director_Insert_Data(req, res, () => {});
 });
-app.get("/admin/Director", (req, res, next) => {
+app.get("/admin/director_data", (req, res, next) => {
   Server_Logic.Admin_Director_Retrive_Data(req, res, () => {});
 });
 app.delete(
-  "/admin/Director/:adminID/:category?/:name?/:questions?",
+  "/admin/director_del/:adminID/:category?/:name?/:questions?",
   (req, res, next) => {
     Server_Logic.Admin_Director_Data_Delete(req, res, () => {});
   }
 );
 //Save-Data
-app.post("/save/EmpoyeeDataKPI", (req, res, next) => {
+app.post("/save/emp_insrt", (req, res, next) => {
   Server_Logic.Save_Employee_Insert_Data(req, res, () => {});
 });
-app.get("/save/EmpoyeeDataKPI/:Empid?/:Value?/:Name?", (req, res, next) => {
+app.get("/save/emp_data/:Empid?/:Value?/:Name?", (req, res, next) => {
   Server_Logic.Save_Employee_Retrive_Data(req, res, () => {});
 });
-app.put("/save/EmpoyeeDataKPI/:Empid", (req, res, next) => {
+app.put("/save/emp_upd/:Empid", (req, res, next) => {
   Server_Logic.Save_Employee_Data_Update(req, res, () => {});
 });
-app.delete("/save/EmpoyeeDataKPI/:Empid", (req, res, next) => {
+app.delete("/save/emp_del/:Empid", (req, res, next) => {
   Server_Logic.Save_Employee_Data_Delete(req, res, () => {});
 });
-app.post("/save/ManagerDataKPI", (req, res, next) => {
+app.post("/save/manager_insrt", (req, res, next) => {
   Server_Logic.Save_Manager_Insert_Data(req, res, () => {});
 });
-app.get("/save/ManagerDataKPI/:Empid?/:Value?/:Name?", (req, res, next) => {
+app.get("/save/manager_data/:Empid?/:Value?/:Name?", (req, res, next) => {
   Server_Logic.Save_Manager_Retrive_Data(req, res, () => {});
 });
-app.put("/save/ManagerDataKPI/:Empid", (req, res, next) => {
+app.put("/save/manager_upd/:Empid", (req, res, next) => {
   Server_Logic.Save_Manager_Data_Update(req, res, () => {});
 });
-app.delete("/save/ManagerDataKPI/:Empid", (req, res, next) => {
+app.delete("/save/manager_del/:Empid", (req, res, next) => {
   Server_Logic.Save_Manager_Data_Delete(req, res, () => {});
 });
-app.post("/save/DirectorDataKPI", (req, res, next) => {
+app.post("/save/director_insrt", (req, res, next) => {
   Server_Logic.Save_Director_Insert_Data(req, res, () => {});
 });
-app.get("/save/DirectorDataKPI/:Empid?/:Value?/:Name?", (req, res, next) => {
+app.get("/save/director_data/:Empid?/:Value?/:Name?", (req, res, next) => {
   Server_Logic.Save_Director_Retrive_Data(req, res, () => {});
 });
-app.put("/save/DirectorDataKPI/:Empid", (req, res, next) => {
+app.put("/save/director_upd/:Empid", (req, res, next) => {
   Server_Logic.Save_Director_Update_Data(req, res, () => {});
 });
-app.delete("/save/DirectorDataKPI/:Empid", (req, res, next) => {
+app.delete("/save/director_del/:Empid", (req, res, next) => {
   Server_Logic.Save_Director_Delete_Data(req, res, () => {});
 });
 //Employee-Data
-app.post("/api/EmpoyeeDataKPI", (req, res, next) => {
+app.post("/api/emp_insrt", (req, res, next) => {
   Server_Logic.Employee_Insert_Data(req, res, () => {});
 });
-app.get("/api/EmployeeDataKPI/:Empid/:Value?/:Name?", (req, res, next) => {
+app.get("/api/emp_data/:Empid/:Value?/:Name?", (req, res, next) => {
   Server_Logic.Employee_Retrive_Data(req, res, () => {});
 });
-app.put("/api/EmployeeDataKPI/:Empid/:Value?/:Name?", (req, res, next) => {
+app.put("/api/emp_upd/:Empid/:Value?/:Name?", (req, res, next) => {
   Server_Logic.Employee_Data_Update(req, res, () => {});
 });
-app.get("/api/EmployeeAllDataKPI/:Empid?/:Value?/:Name?", (req, res, next) => {
+app.get("/api/emp_all_data/:Empid?/:Value?/:Name?", (req, res, next) => {
   Server_Logic.Employee_All_Data_Retrieve(req, res, () => {});
 });
-app.put("/api/EmployeeStatus/:Empid", (req, res, next) => {
+app.put("/api/emp_status_upd/:Empid", (req, res, next) => {
   Server_Logic.Employee_Status_Update(req, res, () => {});
 });
-app.get("/api/EmployeeAllStatusKPI/:Empid?", (req, res, next) => {
+app.get("/api/emp_all_status_data/:Empid?", (req, res, next) => {
   Server_Logic.Employee_All_Status_Retrieve(req, res, () => {});
 });
 
@@ -114,101 +114,109 @@ exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     let result;
 
-    if (event.path === "/admin/RegisterKPI" && event.httpMethod === "POST") {
+    if (event.path === "/admin/register" && event.httpMethod === "POST") {
       result = Server_Logic.AdminPost(body);
     } else if (
-      event.path === "/admin/LoginKPI" &&
+      event.path === "/admin/login" &&
       event.httpMethod === "POST"
     ) {
       result = Server_Logic.AdminloginPost(body);
     } else if (
-      event.path === "/admin/Employee" &&
+      event.path === "/admin/emp_insrt" &&
       event.httpMethod === "POST"
     ) {
       result = Server_Logic.Admin_Employee_Insert_Data(body);
-    } else if (event.path === "/admin/Employee" && event.httpMethod === "GET") {
+    } else if (event.path === "/admin/emp_data" && event.httpMethod === "GET") {
       result = Server_Logic.Admin_Employee_Retrive_Data(body);
     } else if (
-      event.path === "/admin/Employee/:adminID/:category?/:name?/:questions?" &&
+      event.path === "/admin/emp_del/:adminID/:category?/:name?/:questions?" &&
       event.httpMethod === "DELETE"
     ) {
       result = Server_Logic.Admin_Employee_Delete_Data(body);
-    } else if (event.path === "/admin/Manager" && event.httpMethod === "POST") {
+    } else if (event.path === "/admin/manager_insrt" && event.httpMethod === "POST") {
       result = Server_Logic.Admin_Manager_Insert_Data(body);
-    } else if (event.path === "/admin/Manager" && event.httpMethod === "GET") {
+    } else if (event.path === "/admin/manager_data" && event.httpMethod === "GET") {
       result = Server_Logic.Admin_Manager_Retrive_Data(body);
     } else if (
-      event.path === "/admin/Manager/:adminID/:category?/:name?/:questions?" &&
+      event.path === "/admin/manager_del/:adminID/:category?/:name?/:questions?" &&
       event.httpMethod === "DELETE"
     ) {
       result = Server_Logic.Admin_Manager_Data_Delete(body);
-    } else if (
-      event.path === "/save/EmpoyeeDataKPI" &&
+    } 
+    
+    else if (
+      event.path === "/save/emp_insrt" &&
       event.httpMethod === "POST"
     ) {
       result = Server_Logic.Save_Employee_Insert_Data(body);
     } else if (
-      event.path === "/saveEmpoyeeDataKPI/:Empid?/:Value?/:Name?" &&
+      event.path === "/save/emp_data/:Empid?/:Value?/:Name?" &&
       event.httpMethod === "GET"
     ) {
       result = Server_Logic.Save_Employee_Retrive_Data(body);
     } else if (
-      event.path === "/save/EmpoyeeDataKPI/:Empid" &&
+      event.path === "/save/emp_upd/:Empid" &&
       event.httpMethod === "UPDATE"
     ) {
       result = Server_Logic.Save_Employee_Data_Update(body);
     } else if (
-      event.path === "/save/EmpoyeeDataKPI/:Empid" &&
+      event.path === "/save/emp_del/:Empid" &&
       event.httpMethod === "DELETE"
     ) {
       result = Server_Logic.Save_Employee_Data_Delete(body);
-    } else if (
-      event.path === "/save/ManagerDataKPI" &&
+    } 
+    
+    else if (
+      event.path === "/save/manager_insrt" &&
       event.httpMethod === "POST"
     ) {
       result = Server_Logic.Save_Manager_Insert_Data(body);
     } else if (
-      event.path === "/save/ManagerDataKPI/:Empid?/:Value?/:Name?" &&
+      event.path === "/save/manager_data/:Empid?/:Value?/:Name?" &&
       event.httpMethod === "GET"
     ) {
       result = Server_Logic.Save_Manager_Retrive_Data(body);
     } else if (
-      event.path === "/save/ManagerDataKPI/:Empid" &&
+      event.path === "/save/manager_upd/:Empid" &&
       event.httpMethod === "UPDATE"
     ) {
       result = Server_Logic.Save_Manager_Data_Update(body);
     } else if (
-      event.path === "/save/ManagerDataKPI/:Empid" &&
+      event.path === "/save/manager_del/:Empid" &&
       event.httpMethod === "DELETE"
     ) {
       result = Server_Logic.Save_Manager_Data_Delete(body);
-    } else if (
-      event.path === "/api/EmpoyeeDataKPI" &&
+    }
+    
+    
+    
+    else if (
+      event.path === "/api/emp_insrt" &&
       event.httpMethod === "POST"
     ) {
       result = Server_Logic.Employee_Insert_Data(body);
     } else if (
-      event.path === "/api/EmployeeDataKPI/:Empid/:Value?/:Name?" &&
+      event.path === "/api/emp_data/:Empid/:Value?/:Name?" &&
       event.httpMethod === "GET"
     ) {
       result = Server_Logic.Employee_Retrive_Data(body);
     } else if (
-      event.path === "/api/EmployeeDataKPI/:Empid/:Value?/:Name?" &&
+      event.path === "/api/emp_upd/:Empid/:Value?/:Name?" &&
       event.httpMethod === "UPDATE"
     ) {
       result = Server_Logic.Employee_Data_Update(body);
     } else if (
-      event.path === "/api/EmployeeAllDataKPI/:Empid?/:Value?/:Name?" &&
+      event.path === "/api/emp_all_data/:Empid?/:Value?/:Name?" &&
       event.httpMethod === "GET"
     ) {
       result = Server_Logic.Employee_All_Data_Retrieve(body);
     } else if (
-      event.path === "/api/EmployeeStatus/:Empid" &&
+      event.path === "/api/emp_status_upd/:Empid" &&
       event.httpMethod === "UPDATE"
     ) {
       result = Server_Logic.Employee_Status_Update(body);
     } else if (
-      event.path === "/api/EmployeeAllStatusKPI/:Empid?" &&
+      event.path === "/api/emp_all_status_data/:Empid?" &&
       event.httpMethod === "GET"
     ) {
       result = Server_Logic.Employee_All_Status_Retrieve(body);

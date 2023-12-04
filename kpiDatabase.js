a6e894948d3947ca3d6616bfce0946e4cd059c02
Chinna-Anaparthi <123636616+Chinna-Anaparthi@users.noreply.github.com>
2023-12-04 23:33:05 +0530

    port adding

--- kpiDatabase.js
+++ kpiDatabase.js
@@ -5,7 +5,8 @@ const DataBaseConnection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database:process.env.DATABASE
+    database:process.env.DATABASE,
+    port:process.env.PORT
 });
 DataBaseConnection.connect((err,res)=>
 {
